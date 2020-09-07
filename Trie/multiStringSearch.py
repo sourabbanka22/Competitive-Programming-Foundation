8cb959931cf95d60a7fadee11c111adc2fada655
sourabbanka22 <sourabbanka22@gmail.com>
2020-09-07 17:27:15 +0530

    multiStringSearch

--- Trie/multiStringSearch.py
+++ Trie/multiStringSearch.py
@@ -1,26 +1,53 @@
 class Trie:
-    def __init__(self, string):
+    def __init__(self):
         self.root = {}
         self.endSymbol = "*"
-        self.populateSuffixTrieFrom(string)
 
     def populateTrieFrom(self, string):
         # Write your code here.
-		current = self.root
+        current = self.root
         for char in string:
-			if char not in current:
-				current[char] = {}
-			current = current[char]
-		current[self.endSymbol] = True
-				
+            if char not in current:
+                current[char] = {}
+            current = current[char]
+            current[self.endSymbol] = True
+        
+        current = self.root
+        for first in range(len(string)):
+            for second in range(first, len(string)):
+                if string[second] not in current:
+                    current[string[second]] = {}
+                current = current[string[second]]
+            current[self.endSymbol] = True
+            current = self.root
+                
 
     def contains(self, string):
         # Write your code here.
-        pass
+        current = self.root
+        for char in string:
+            if char not in current:
+                return False
+            current = current[char]
+        
+        return self.endSymbol in current
 
 def multiStringSearch(bigString, smallStrings):
     # Write your code here.
     bigStringList = bigString.split(" ")
-    return bigStringList
+    trie = Trie()
+
+    for string in bigStringList:
+        trie.populateTrieFrom(string)
+    
+    contains = []
+
+    for string in smallStrings:
+        if trie.contains(string):
+            contains.append(True)
+        else:
+            contains.append(False)
+
+    return contains
 
 print(multiStringSearch("this is a big string", ["this", "yo", "is", "a", "bigger", "string", "kappa"]))
