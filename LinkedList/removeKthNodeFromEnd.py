47b111cd233325cc982017bad7e5581ac57d9516
sourabbanka22 <sourabbanka22@gmail.com>
2020-09-10 15:22:21 +0530

    removeKthNodeFromEnd Refactored

--- LinkedList/removeKthNodeFromEnd.py
+++ LinkedList/removeKthNodeFromEnd.py
@@ -1,4 +1,3 @@
-# This is an input class. Do not edit.
 class LinkedList:
     def __init__(self, value):
         self.value = value
@@ -6,18 +5,17 @@ class LinkedList:
 
 
 def removeKthNodeFromEnd(head, k):
-    # Write your code here.
-	behind = head
-	front = head
-	counter = 1
-	while counter <= k:
-		front = front.next
-		counter += 1
-	if front == None:
-		head.value = head.next.value
-		head.next = head.next.next
-		return
-	while front.next is not None:
-		front = front.next
-		behind = behind.next
-	behind.next = behind.next.next
+    behind = head
+    front = head
+    counter = 1
+    while counter <= k:
+        front = front.next
+        counter += 1
+    if front == None:
+        head.value = head.next.value
+        head.next = head.next.next
+        return
+    while front.next is not None:
+        front = front.next
+        behind = behind.next
+    behind.next = behind.next.next
