cd89d114e1957b9bdbad69f574c19129979fe98a
sourabbanka22 <sourabbanka22@gmail.com>
2020-09-18 12:15:11 +0530

    interweavingStrings

---
+++ Recursion/interweavingStrings.py
@@ -0,0 +1,24 @@
+def interweavingStrings(one, two, three):
+    # Write your code here.
+    if len(three) != len(one) + len(two):
+        return False
+    
+    return interweavingStringsUtil(0, 0, one, two, three)
+
+def interweavingStringsUtil(idxOne, idxTwo, one, two, three):
+    idxThree = idxOne + idxTwo
+    if idxThree == len(three):
+        return True
+    
+    if idxOne<len(one) and one[idxOne] == three[idxThree]:
+        if interweavingStringsUtil(idxOne+1, idxTwo, one, two, three):
+            return True
+    
+    if idxTwo<len(two) and two[idxTwo] == three[idxThree]:
+        return interweavingStringsUtil(idxOne, idxTwo+1, one, two, three)
+    
+    return False
+
+print(interweavingStrings("algoexpert", "your-dream-job", "your-algodream-expertjob"))
+
+print(interweavingStrings("aabcc", "dbbca", "aadbbcbcac"))
