15b9815892c5d64b70f92ed381ee40a390a48cea
sourabbanka22 <sourabbanka22@gmail.com>
2020-09-28 13:47:03 +0530

    squareOfZeroes n^3 with Space Compromise

--- Dynamic Programming/squareOfZeroes.py
+++ Dynamic Programming/squareOfZeroes.py
@@ -1,31 +1,50 @@
 def squareOfZeroes(matrix):
     # Write your code here.
-    for row in range(len(matrix)):
-        for col in range(len(matrix[0])):
-            for lowestRow in range(row+1, len(matrix)):
-                for rightMostCol in range(col+1, len(matrix[0])):
-                    innerRow = row
-                    innerCol = col+1
-                    print(innerRow, lowestRow, innerCol, rightMostCol)
-                    if matrix[row][col]==1:
-                        continue
-                    while innerRow<=lowestRow and innerCol<=rightMostCol:
-                        print("Inside: ", innerRow, innerCol)
-                        if innerRow==row and innerCol==col:
-                            print("Its ME", innerRow, innerCol)
-                            return True
-                        
-                        if matrix[innerRow][innerCol]==1:
-                            break
-                        elif innerCol == col:
-                            innerRow -= 1
-                        elif innerRow == lowestRow:
-                            innerCol -= 1
-                        elif innerCol == rightMostCol:
-                            innerRow += 1
-                        elif innerRow == row:
-                            innerCol += 1
+    infoMatrix = preComputeValues(matrix)
+    size  = len(matrix)
+    for topRow in range(size):
+        for leftCol in range(size):
+            squareLength = 2
+            while squareLength<=size-leftCol and squareLength<=size-topRow:
+                bottomRow = topRow+squareLength-1
+                rightCol = leftCol+squareLength-1
+                if isSquareOfZeroes(infoMatrix, topRow, leftCol, bottomRow, rightCol):
+                    return True
+                squareLength+=1
     return False
+
+def isSquareOfZeroes(infoMatrix, r1, c1, r2, c2):
+    squareLength = r2-r1+1
+    hasTopBorder = infoMatrix[r1][c1]["numZeroesRight"] >= squareLength
+    hasLeftBorder = infoMatrix[r1][c1]["numZeroesBelow"] >= squareLength
+    hasBottomBorder = infoMatrix[r2][c1]["numZeroesRight"] >= squareLength
+    hasRightBorder = infoMatrix[r1][c2]["numZeroesBelow"] >= squareLength
+
+    return hasTopBorder and hasLeftBorder and hasBottomBorder and hasRightBorder
+
+def preComputeValues(matrix):
+    infoMatrix = [[None for _ in row] for row in matrix]
+    size = len(matrix)
+
+    for row in range(size):
+        for col in range(size):
+            numZeroes = 1 if matrix[row][col]==0 else 0
+            infoMatrix[row][col] = {
+                "numZeroesBelow": numZeroes,
+                "numZeroesRight": numZeroes,
+            }
+    
+    lastIdx = len(matrix)-1
+    for row in reversed(range(size)):
+        for col in reversed(range(size)):
+            if matrix[row][col] == 1:
+                continue
+            if row<lastIdx:
+                infoMatrix[row][col]["numZeroesBelow"] += infoMatrix[row+1][col]["numZeroesBelow"]
+            if col<lastIdx:
+                infoMatrix[row][col]["numZeroesRight"] += infoMatrix[row][col+1]["numZeroesRight"]
+
+    return infoMatrix
 
 matrix1 = [[1, 1, 0, 1], 
             [1, 0, 0, 1], 
@@ -40,5 +59,15 @@ matrix2 = [
     [0, 1, 1, 1, 0, 1],
     [0, 0, 0, 0, 0, 1]
   ]
+
+# funMatrix = [[16, 15, 14, 13], 
+#             [12, 11, 10, 9], 
+#             [8, 7, 6, 5], 
+#             [4, 3, 2, 1]]
+# funList = []
+# for row in reversed(range(len(funMatrix))):
+#     for col in reversed(range(len(funMatrix[0]))):
+#         funList.append(funMatrix[row][col])
+# print(funList)
 
 print(squareOfZeroes(matrix2))
